4e1475c900621f9c42ec2e706bcc85134f4136a8
Mattias Baldi <mattiasbaldi@gmail.com>
2024-12-14 07:08:15 +0100

    added LoadingScreen

--- src/Experience/Utils/Resources.js
+++ src/Experience/Utils/Resources.js
@@ -2,6 +2,7 @@ import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { RGBELoader } from 'three/examples/jsm/Addons.js'
 import EventEmitter from './EventEmitter.js'
+import { gsap } from 'gsap'
 
 export default class Resources extends EventEmitter
 {
@@ -14,18 +15,50 @@ export default class Resources extends EventEmitter
         this.toLoad = this.sources.length
         this.loaded = 0
 
+        this.setLoadingScreen()
         this.setLoaders()
         this.startLoading()
+
+    }
+
+    setLoadingScreen()
+    {
+       
+        console.log('test123')
+        const overLay = document.querySelector('.loading.overlay')
+        console.log('before LoadingManager')
+        
+        this.loadingManager = new THREE.LoadingManager(
+            // Loaded 
+        () =>
+    {
+        console.log('before gsap')
+        gsap.delayedCall(0.5, () =>
+        {
+            console.log('gsap delay')
+            gsap.to(overLay, { duration: 3, opacity: 0, onComplete: () => overLay.style.display = 'none' })
+            console.log('has Loaded')
+        })
+    },
+
+    // Progress 
+    (itemUrl, itemsLoaded, itemsTotal) =>
+        {
+        const progressRatio = itemsLoaded / itemsTotal
+        console.log(`load ${progressRatio}`)
+        overLay.innerHTML = `${progressRatio * 100 + '%'}`
+    });
+
     }
 
     setLoaders()
     {
         this.loaders = {}
-        this.loaders.gltfLoader = new GLTFLoader()
-        this.loaders.textureLoader = new THREE.TextureLoader()
-        this.loaders.cubeTextureLoader = new THREE.CubeTextureLoader()
-        this.loaders.rgbeLoader = new RGBELoader()
-        this.loaders.audioLoader = new THREE.AudioLoader();
+        this.loaders.gltfLoader = new GLTFLoader(this.loadingManager);
+        this.loaders.textureLoader = new THREE.TextureLoader(this.loadingManager);
+        this.loaders.cubeTextureLoader = new THREE.CubeTextureLoader(this.loadingManager);
+        this.loaders.rgbeLoader = new RGBELoader(this.loadingManager);
+        this.loaders.audioLoader = new THREE.AudioLoader(this.loadingManager);
     }
 
     startLoading()
@@ -86,6 +119,7 @@ export default class Resources extends EventEmitter
         }
     }
 
+
     sourceLoaded(source, file)
     {
         this.items[source.name] = file
@@ -97,4 +131,5 @@ export default class Resources extends EventEmitter
             this.trigger('ready')
         }
     }
+
 }
